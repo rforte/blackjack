67662acaf194173555603ae5e32b6f30106c4411
Nick Forte <nick@wise-view.com>
2012-10-12 17:36:09 -0700

    moving from rails c to specs

--- spec/controllers/api/v1/game_controller_spec.rb
+++ spec/controllers/api/v1/game_controller_spec.rb
@@ -18,10 +18,40 @@ describe Api::V1::GameController do
     response.response_code.should eq(200)
   end
   
-  it "should tell user game is over if they try to hit or stand when the game is over" do
+  # it "should tell user game is over if they try to hit or stand when the game is over" do
+  #   post :update, {:id => @completed.id, :cmd => 'hit'}
+  #   response.response_code.should eq(Api::V1::GameErrors::GAME_ALREADY_COMPLETED[:status])
+  #   json = JSON::parse(response.body)
+  #   json['code'].should eq(Api::V1::GameErrors::GAME_ALREADY_COMPLETED[:code])
+  # end
+
+  it "if game is complete then just return the status of the game" do
+    golden = JSON::parse GamePresenter.new(@completed).to_json
     post :update, {:id => @completed.id, :cmd => 'hit'}
-    response.response_code.should eq(Api::V1::GameErrors::GAME_ALREADY_COMPLETED[:status])
+    response.response_code.should eq(200)
     json = JSON::parse(response.body)
-    json['code'].should eq(Api::V1::GameErrors::GAME_ALREADY_COMPLETED[:code])
-  end  
+    json.should eq(golden)
+  end
+  
+  it "should report game not found on invalid gid" do
+    post :show, :id => '1234'
+    response.response_code.should eq(404)
+    json = JSON::parse(response.body)
+    json['code'].should eq(Api::V1::GameErrors::GAME_NOT_FOUND[:code])
+  end
+  
+  it "should return an error if invalid cmd" do
+    post :update, {:id => @game, :cmd => 'xxx'}
+    response.response_code.should eq(403)
+    json = JSON::parse(response.body)
+    json['code'].should eq(Api::V1::GameErrors::INVALID_ACTION[:code])
+  end
+
+  it "should return an error if no cmd" do
+    post :update, {:id => @game}
+    response.response_code.should eq(403)
+    json = JSON::parse(response.body)
+    json['code'].should eq(Api::V1::GameErrors::MUST_SUPPLY_ACTION[:code])
+  end
+
 end
